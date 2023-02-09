0038a156c94e9b76fb9280993d569328f5e53907
Justin Farnsworth <farnswj1@tcnj.edu>
2023-02-09 15:06:38 -0500

    cleaned up the code

--- app/App.py
+++ app/App.py
@@ -1,82 +1,64 @@
 import streamlit as st
 from PIL import Image
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup as bs4
 from urllib.request import urlopen
 from newspaper import Article
 import io
 import nltk
+
 nltk.download('punkt')
 
-st.set_page_config(page_title='InNews🇮🇳: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
+st.set_page_config(page_title='InNews 🇮🇳: A Summarised News 📰 Portal', page_icon='./Meta/newspaper.ico')
 
 
-def fetch_news_search_topic(topic):
-    site = 'https://news.google.com/rss/search?q={}'.format(topic)
-    op = urlopen(site)  # Open that site
-    rd = op.read()  # read data from site
-    op.close()  # close the object
-    sp_page = soup(rd, 'xml')  # scrapping data from site
-    news_list = sp_page.find_all('item')  # finding news
-    return news_list
+def extract_news_from_url(url):
+    with urlopen(url) as page:  # Open that site and close when finished
+        return page.read()  # read data from site
 
 
-def fetch_top_news():
-    site = 'https://news.google.com/news/rss'
-    op = urlopen(site)  # Open that site
-    rd = op.read()  # read data from site
-    op.close()  # close the object
-    sp_page = soup(rd, 'xml')  # scrapping data from site
-    news_list = sp_page.find_all('item')  # finding news
-    return news_list
+def fetch_news_list(url):
+    news = extract_news_from_url(url)
+    sp_page = bs4(news, 'xml')  # scrapping data from site
+    return sp_page.find_all('item')  # finding news
 
 
-def fetch_category_news(topic):
-    site = 'https://news.google.com/news/rss/headlines/section/topic/{}'.format(topic)
-    op = urlopen(site)  # Open that site
-    rd = op.read()  # read data from site
-    op.close()  # close the object
-    sp_page = soup(rd, 'xml')  # scrapping data from site
-    news_list = sp_page.find_all('item')  # finding news
-    return news_list
-
-
-def fetch_news_poster(poster_link):
+def fetch_news_poster(url):
     try:
-        u = urlopen(poster_link)
-        raw_data = u.read()
-        image = Image.open(io.BytesIO(raw_data))
-        st.image(image, use_column_width=True)
+        raw_data = extract_news_from_url(url)
+        filepath = io.BytesIO(raw_data)
     except:
-        image = Image.open('./Meta/no_image.jpg')
-        st.image(image, use_column_width=True)
+        filepath = './Meta/no_image.jpg'
+
+    image = Image.open(filepath)
+    st.image(image, use_column_width=True)
 
 
 def display_news(list_of_news, news_quantity):
-    c = 0
-    for news in list_of_news:
-        c += 1
-        # st.markdown(f"({c})[ {news.title.text}]({news.link.text})")
-        st.write('**({}) {}**'.format(c, news.title.text))
+    for news, index in zip(list_of_news, range(1, news_quantity + 1)):
+        st.write(f'**({index}) {news.title.text}**')
         news_data = Article(news.link.text)
+
         try:
             news_data.download()
             news_data.parse()
             news_data.nlp()
         except Exception as e:
             st.error(e)
+
         fetch_news_poster(news_data.top_image)
+
         with st.expander(news.title.text):
             st.markdown(
-                '''<h6 style='text-align: justify;'>{}"</h6>'''.format(news_data.summary),
-                unsafe_allow_html=True)
-            st.markdown("[Read more at {}...]({})".format(news.source.text, news.link.text))
-        st.success("Published Date: " + news.pubDate.text)
-        if c >= news_quantity:
-            break
+                f'''<h6 style='text-align: justify;'>{news_data.summary}"</h6>''',
+                unsafe_allow_html=True
+            )
+            st.markdown(f"[Read more at {news.source.text}...]({news.link.text})")
+
+        st.success(f"Published Date: {news.pubDate.text}")
 
 
-def run():
-    st.title("InNews🇮🇳: A Summarised News📰")
+def main():
+    st.title("InNews 🇮🇳: A Summarized News 📰")
     image = Image.open('./Meta/newspaper.png')
 
     col1, col2, col3 = st.columns([3, 5, 3])
@@ -89,46 +71,56 @@ def run():
 
     with col3:
         st.write("")
-    category = ['--Select--', 'Trending🔥 News', 'Favourite💙 Topics', 'Search🔍 Topic']
+
+    category = ('--Select--', 'Trending🔥 News', 'Favourite💙 Topics', 'Search🔍 Topic')
     cat_op = st.selectbox('Select your Category', category)
+    
     if cat_op == category[0]:
         st.warning('Please select Type!!')
     elif cat_op == category[1]:
         st.subheader("✅ Here is the Trending🔥 news for you")
         no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
-        news_list = fetch_top_news()
+        url = 'https://news.google.com/news/rss'  # top news
+        news_list = fetch_news_list(url)
         display_news(news_list, no_of_news)
     elif cat_op == category[2]:
-        av_topics = ['Choose Topic', 'WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS', 'SCIENCE',
-                     'HEALTH']
+        av_topics = (
+            'Choose Topic', 'WORLD', 'NATION',
+            'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT',
+            'SPORTS', 'SCIENCE', 'HEALTH'
+        )
         st.subheader("Choose your favourite Topic")
         chosen_topic = st.selectbox("Choose your favourite Topic", av_topics)
+        
         if chosen_topic == av_topics[0]:
             st.warning("Please Choose the Topic")
         else:
             no_of_news = st.slider('Number of News:', min_value=5, max_value=25, step=1)
-            news_list = fetch_category_news(chosen_topic)
+            url = f'https://news.google.com/news/rss/headlines/section/topic/{chosen_topic}'  # category news
+            news_list = fetch_news_list(url)
+
             if news_list:
-                st.subheader("✅ Here are the some {} News for you".format(chosen_topic))
+                st.subheader(f"✅ Here are the some {chosen_topic} News for you")
                 display_news(news_list, no_of_news)
             else:
-                st.error("No News found for {}".format(chosen_topic))
-
+                st.error(f"No News found for {chosen_topic}")
     elif cat_op == category[3]:
-        user_topic = st.text_input("Enter your Topic🔍")
+        user_topic = st.text_input("Enter your Topic 🔍")
         no_of_news = st.slider('Number of News:', min_value=5, max_value=15, step=1)
 
-        if st.button("Search") and user_topic != '':
+        if st.button("Search") and user_topic:
             user_topic_pr = user_topic.replace(' ', '')
-            news_list = fetch_news_search_topic(topic=user_topic_pr)
+            url = f'https://news.google.com/rss/search?q={user_topic_pr}'  # search topics
+            news_list = fetch_news_list(url)
+
             if news_list:
-                st.subheader("✅ Here are the some {} News for you".format(user_topic.capitalize()))
+                st.subheader(f"✅ Here are the some {user_topic.capitalize()} News for you")
                 display_news(news_list, no_of_news)
             else:
-                st.error("No News found for {}".format(user_topic))
+                st.error(f"No news found for {user_topic}")
         else:
-            st.warning("Please write Topic Name to Search🔍")
+            st.warning("Please write Topic Name to Search 🔍")
 
 
 if __name__ == '__main__':
-    run()
+    main()
